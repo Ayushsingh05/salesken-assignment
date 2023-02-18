8dbddee1de22ad657d5a25cbb11c575e6f1658a7
Ayushsingh05 <singhayush28321@gmail.com>
2023-02-18 13:48:55 +0530

    data collected

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
 
+const loggedIn=localStorage.getItem('loggedIn')|| false;
+const user= JSON.parse(localStorage.getItem("userDetails"))||{};
+console.log(loggedIn);
+if(loggedIn){
+document.getElementById("login_logout_button").innerText="Logout";
+document.getElementById("nav-username").innerText=user.username.split(" ")[0];
+}
+else{
+    document.getElementById("login_logout_button").innerText="Login";
+    document.getElementById("quiz_start_button").disabled=true; 
+}
+
+const  handleStartQuiz= (e)=>{
+    console.log(e.target);
+}
+document.getElementById("quiz_start_button").addEventListener("click",handleStartQuiz)
---
+++ quizData/data.js
@@ -0,0 +1,138 @@
+const data = [
+  
+  {
+    question: "What is the capital of Japan?",
+    choices: ["Kyoto", "Osaka", "Tokyo", "Hiroshima"],
+    answer: "Tokyo",
+  },
+  {
+    question: "Who is the current President of the United States?",
+    choices: ["Barack Obama", "Joe Biden", "Donald Trump", "Hillary Clinton"],
+    answer: "Joe Biden",
+  },
+  {
+    question: "What is the largest country in the world by land area?",
+    choices: ["China", "India", "Russia", "United States"],
+    answer: "Russia",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    choices: [
+      "Michelangelo",
+      "Leonardo da Vinci",
+      "Vincent van Gogh",
+      "Pablo Picasso",
+    ],
+    answer: "Leonardo da Vinci",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    choices: ["Venus", "Mars", "Jupiter", "Saturn"],
+    answer: "Jupiter",
+  },
+  {
+    question: 'Who wrote the book "To Kill a Mockingbird"?',
+    choices: [
+      "Harper Lee",
+      "J.K. Rowling",
+      "Ernest Hemingway",
+      "William Shakespeare",
+    ],
+    answer: "Harper Lee",
+  },
+  {
+    question: "Who invented the telephone?",
+    choices: [
+      "Thomas Edison",
+      "Alexander Graham Bell",
+      "Albert Einstein",
+      "Isaac Newton",
+    ],
+    answer: "Alexander Graham Bell",
+  },
+  {
+    question: "What is the capital of Italy?",
+    choices: ["Rome", "Florence", "Venice", "Milan"],
+    answer: "Rome",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    choices: ["Au", "Ag", "Fe", "Cu"],
+    answer: "Au",
+  },
+  {
+    question: "Who was the first person to step on the moon?",
+    choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "John Glenn"],
+    answer: "Neil Armstrong",
+  },
+  {
+    question: "What is the largest ocean in the world?",
+    choices: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean",
+    ],
+    answer: "Pacific Ocean",
+  },
+  {
+    question: 'Who is the author of the "Harry Potter" series?',
+    choices: ["J.K. Rowling", "Stephen King", "Roald Dahl", "Dan Brown"],
+    answer: "J.K. Rowling",
+  },
+  {
+    question: "What is the largest desert in the world?",
+    choices: [
+      "Sahara Desert",
+      "Gobi Desert",
+      "Arabian Desert",
+      "Kalahari Desert",
+    ],
+    answer: "Sahara Desert",
+  },
+  {
+    question: "Who is the founder of Microsoft?",
+    choices: ["Bill Gates", "Steve Jobs", "Mark Zuckerberg", "Jeff Bezos"],
+    answer: "Bill Gates",
+  },
+  {
+    question: "What is the smallest country in the world?",
+    choices: ["Monaco", "Vatican City", "Liechtenstein", "San Marino"],
+    answer: "Vatican City",
+  },
+  {
+    question: 'Who directed the movie "The Godfather"?',
+    choices: [
+      "Martin Scorsese",
+      "Francis Ford Coppola",
+      "Steven Spielberg",
+      "Alfred Hitchcock",
+    ],
+    answer: "Francis Ford Coppola",
+  },
+  {
+    question: "What is the currency of Japan?",
+    choices: ["Dollar", "Euro", "Pound", "Yen"],
+    answer: "Yen",
+  },
+  {
+    question: "Who is the current Prime Minister of Canada?",
+    choices: [
+      "Justin Trudeau",
+      "Stephen Harper",
+      "Jean Chrétien",
+      "Paul Martin",
+    ],
+    answer: "Justin Trudeau",
+  },
+  {
+    question: "What is the highest mountain in the world?",
+    choices: ["K2", "Mount Everest", "Makalu", "Kangchenjunga"],
+    answer: "Mount Everest",
+  },
+  {
+    question: 'Who is the author of the "Lord of the Rings" trilogy?',
+    choices: ["J.R.R. Tolkien", "C.S. Lewis", "J.K. Rowling", "Stephen King"],
+    answer: "J.R.R. Tolkien",
+  },
+];
